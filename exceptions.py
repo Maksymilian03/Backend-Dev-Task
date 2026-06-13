384eb22132329c5ff6a8fbf444ced93c0d8fb8da
Maksymilian03 <maksiu2003xd@gmail.com>
2026-06-13 15:13:08 +0200

    feat: add InvalidInputError to custom exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,8 @@
 class NoValidOffsetError(Exception):
     """Raised when no rafter offset satisfies mounting constraints."""
     pass
+
+
+class InvalidInputError(Exception):
+    """Raised when input data has invalid structure or types."""
+    pass
